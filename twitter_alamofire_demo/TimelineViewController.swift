3f620baec9e2b7436790d69ca5114421309dc378
NancyGomez <nagomez@csumb.edu>
2018-03-05 14:20:48 -0800

    Added pull to refresh

--- twitter_alamofire_demo/TimelineViewController.swift
+++ twitter_alamofire_demo/TimelineViewController.swift
@@ -17,20 +17,35 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(TimelineViewController.refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
         tableView.dataSource = self
         tableView.delegate = self
         
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 100
         
-        APIManager.shared.getHomeTimeLine { (tweets, error) in
-            if let tweets = tweets {
+        getTweets()
+    }
+    func getTweets()
+    {
+        APIManager.shared.getHomeTimeLine{(tweets, error) in
+            if let tweets = tweets{
                 self.tweets = tweets
                 self.tableView.reloadData()
-            } else if let error = error {
-                print("Error getting home timeline: " + error.localizedDescription)
+            }
+            else if let error = error {
+                print("Error getting tweets: " + error.localizedDescription )
             }
         }
+    }
+    func refreshControlAction(_ refreshControl: UIRefreshControl){
+        getTweets()
+        tableView.reloadData()
+        refreshControl.endRefreshing()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
